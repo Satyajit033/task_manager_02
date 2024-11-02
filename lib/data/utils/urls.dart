02e2f8fd925ef0c89d97299ffa2c558ab2a49b68
Satyajit033 <satyajitbiswas033@gmail.com>
2024-11-02 16:22:48 +0600

    task_manager_02 project completed and some api link changs and some yaml file changes has been done

--- lib/data/utils/urls.dart
+++ lib/data/utils/urls.dart
@@ -1,7 +1,7 @@
 class Urls {
   Urls._();
 
-  static const String _baseUrl = 'https://task.teamrabbil.com/api/v1';
+/*  static const String _baseUrl = 'https://task.teamrabbil.com/api/v1';
   static String registration = '$_baseUrl/registration';
   static String login = '$_baseUrl/login';
   static String createTask = '$_baseUrl/createTask';
@@ -19,5 +19,27 @@ class Urls {
   static String updateProfile = '$_baseUrl/profileUpdate';
   static String sendOtpToEmail(String email) => '$_baseUrl/RecoverVerifyEmail/$email';
   static String otpVerify(String email, String otp) => '$_baseUrl/RecoverVerifyOTP/$email/$otp';
-  static String resetPassword = '$_baseUrl/RecoverResetPass';
+  static String resetPassword = '$_baseUrl/RecoverResetPass';*/
+
+  static const String _baseUrl = 'http://35.73.30.144:2005/api/v1';
+  static String registration = '$_baseUrl/Registration';
+  static String login = '$_baseUrl/Login';
+  static String createTask = '$_baseUrl/createTask';
+  static String taskStatusCount = '$_baseUrl/taskStatusCount';
+  static String newTasks = '$_baseUrl/listTaskByStatus/New';
+  static String inProgressTasks = '$_baseUrl/listTaskByStatus/Progress';
+  static String canceledTasks = '$_baseUrl/listTaskByStatus/Canceled';
+  static String completedTasks = '$_baseUrl/listTaskByStatus/Completed';
+
+  static String deleteTask(String id) => '$_baseUrl/deleteTask/$id';
+
+  static String updateTask(String id, String status) =>
+      '$_baseUrl/updateTaskStatus/$id/$status';
+
+  static String updateProfile = '$_baseUrl/ProfileUpdate';
+  static String sendOtpToEmail(String email) => '$_baseUrl/RecoverVerifyEmail/$email';
+  static String otpVerify(String email, String otp) => '$_baseUrl/RecoverVerifyOtp/$email/$otp';
+  static String resetPassword = '$_baseUrl/RecoverResetPassword';
+
+
 }
